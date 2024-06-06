0cf3e7824629c51ad19c60ce90d88aa70e99f8b3
codebangbang <ryfair@gmail.com>
2024-06-06 14:47:14 -0400

    Tests PASS!

--- guessing-game.js
+++ guessing-game.js
@@ -1,10 +1,16 @@
 function guessingGame() {
   let answer = Math.floor(Math.random() * 100);
+  let guesses = 0;
+  let gameOver = false;
 
   return function guess(num) {
+    if (gameOver) return `The game is over, you already won!`;
+    guesses++;
     if (num === answer) {
-      return `You got it!`;
+      gameOver = true;
+      return `You win! You found ${answer} in ${guesses} guesses.`;
     }
+    
     if (num < answer) return `${num} is too low!`;
     if (num > answer) return `${num} is too high!`;
   };
