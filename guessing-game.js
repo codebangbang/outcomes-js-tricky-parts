083234eadac42b81c4250d414df71b17281ee900
codebangbang <ryfair@gmail.com>
2024-06-06 13:58:22 -0400

    Guessing Game Script created

--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,13 @@
 function guessingGame() {
+  let answer = Math.floor(Math.random() * 100);
 
+  return function guess(num) {
+    if (num === answer) {
+      return `You got it!`;
+    }
+    if (num < answer) return `${num} is too low!`;
+    if (num > answer) return `${num} is too high!`;
+  };
 }
 
 module.exports = { guessingGame };
